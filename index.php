0cd1306fd6f41da36b809de353ad2034b992e7ae
Paul <neagapaul11@gmail.com>
2017-01-04 12:10:35 +0200

    adaugare imagine

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ and open the template in the editor.
         echo "hi!";
         echo 'TEST';
         echo 'Paul';
+		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTheUznVGz9G35nefi74xEZrtuwoVEreFTPw2rJbWsz2pAWOMe6" height="100" width="100">
         echo "hi! again";
         echo "Modificarea mea!";
         ?>
